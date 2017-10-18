80df792e553c9148ed1f5de15c300852556f1d8d
tangert <tyler@angert.com>
2017-10-18 15:03:49 -0400

    some icon changes

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,9 +1,15 @@
 import React, { PropTypes } from 'react'
 import './Header.css'
+import logo from './../../static/logo.svg'
 
 const Header = (props) => {
   return (
     <div className = "app-header">
+
+      <a className = "elit-link" href = "https://elit.cloud/" target="_blank">
+        <img style = {{width: "25px", height: "25px", marginRight: "10px"}} src={logo}/>
+      </a>
+
       <div className = "app-header-title">ELIT Sentiment Visualization</div>
     </div>
   )
